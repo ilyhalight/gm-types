7d3606e686f083dd7f22102b22da47126753ba68
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-03-28 01:39:54 +0300

    feat: added gm storage api types

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,4 +10,59 @@ declare namespace GM {
     onclick?: () => void
   ): undefined;
   function notification(options: GMNotificationOptions): undefined;
+
+  /**
+   * @available Greasemonkey, Tampermonkey, Violetmonkey, UserScripts, AdGuard, Firemonkey
+   * @note In Firemonkey, if grant has GM_getValue and GM.getValue, GM_getValue becomes unvailable
+   */
+  function getValue<T = unknown>(
+    key: string,
+    defaultValue?: unknown
+  ): Promise<T>;
+
+  /**
+   * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+   */
+  function getValues<T = Record<string, KeysOrDefaultValue>>(
+    keysOrDefault: string[]
+  ): Promise<T>;
+  /**
+   * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+   */
+  function getValues<T = Record<string, KeysOrDefaultValue>>(
+    keysOrDefault: T
+  ): Promise<T>;
+
+  /**
+   * @available Greasemonkey, Tampermonkey, Violetmonkey, UserScripts, AdGuard, Firemonkey
+   * @note In Firemonkey, if grant has GM_setValue and GM.setValue, GM_setValue becomes unvailable
+   */
+  function setValue<T = string | boolean | number | undefined>(
+    key: string,
+    value: T
+  ): Promise<undefined>;
+
+  /**
+   * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+   */
+  function setValues<
+    T = Record<string, null | object | string | number | undefined | boolean>
+  >(keysOrDefault: T): Promise<undefined>;
+
+  /**
+   * @available Greasemonkey, Tampermonkey, Violetmonkey, UserScripts, AdGuard, Firemonkey
+   * @note In Firemonkey, if grant has GM_deleteValue and GM.deleteValue, GM_deleteValue becomes unvailable
+   */
+  function deleteValue(key: string): Promise<undefined>;
+
+  /**
+   * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+   */
+  function deleteValues(keys: string[]): Promise<undefined>;
+
+  /**
+   * @available Greasemonkey, Tampermonkey, Violetmonkey, UserScripts, AdGuard, Firemonkey
+   * @note In Firemonkey, if grant has GM_listValues and GM.listValues, GM_listValues becomes unvailable
+   */
+  function listValues<T extends string = string>(): Promise<T[]>;
 }
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
 import type { GMInfo } from "./info";
 import type { GMNotificationOptions } from "./notification";
 
+type KeysOrDefaultValue = null | object | string | number | undefined | boolean;
+
 declare const GM_info: GMInfo;
 
 /**
@@ -17,3 +19,62 @@ declare function GM_notification(
  * @available Tampermonkey, Violetmonkey
  */
 declare function GM_notification(options: GMNotificationOptions): undefined;
+
+/**
+ * @available Greasemonkey, Tampermonkey, Violetmonkey, AdGuard, OrangeMonkey, Firemonkey
+ * @note In Firemonkey, if grant has GM_getValue and GM.getValue, GM_getValue becomes unvailable
+ * @warning ❌ UserScripts
+ */
+declare function GM_getValue<T = unknown>(
+  key: string,
+  defaultValue?: unknown
+): T;
+
+/**
+ * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+ */
+declare function GM_getValues<T = Record<string, KeysOrDefaultValue>>(
+  keysOrDefault: string[]
+): T;
+/**
+ * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+ */
+declare function GM_getValues<T = Record<string, KeysOrDefaultValue>>(
+  keysOrDefault: T
+): T;
+
+/**
+ * @available Greasemonkey, Tampermonkey, Violetmonkey, AdGuard, OrangeMonkey, Firemonkey
+ * @note In Firemonkey, if grant has GM_setValue and GM.setValue, GM_setValue becomes unvailable
+ * @warning ❌ UserScripts
+ */
+declare function GM_setValue<T = string | boolean | number | undefined>(
+  key: string,
+  value: T
+): undefined;
+
+/**
+ * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+ */
+declare function GM_setValues<T = Record<string, KeysOrDefaultValue>>(
+  keysOrDefault: T
+): undefined;
+
+/**
+ * @available Greasemonkey, Tampermonkey, Violetmonkey, AdGuard, OrangeMonkey, Firemonkey
+ * @note In Firemonkey, if grant has GM_deleteValue and GM.deleteValue, GM_deleteValue becomes unvailable
+ * @warning ❌ UserScripts
+ */
+declare function GM_deleteValue(key: string): undefined;
+
+/**
+ * @available Tampermonkey 5.3+, Violetmonkey 2.19.1+
+ */
+declare function GM_deleteValues(keys: string[]): undefined;
+
+/**
+ * @available Greasemonkey, Tampermonkey, Violetmonkey, AdGuard, OrangeMonkey, Firemonkey
+ * @note In Firemonkey, if grant has GM_listValues and GM.listValues, GM_listValues becomes unvailable
+ * @warning ❌ UserScripts
+ */
+declare function GM_listValues<T extends string = string>(): T[];
